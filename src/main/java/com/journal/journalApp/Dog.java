6edf1f926fe6c53243a4fa2afb4196c7debe85a9
imhrsit <tiwariharsh1408@gmail.com>
2024-12-06 15:24:19 +0530

    component scan + configuration

---
+++ src/main/java/com/journal/journalApp/Dog.java
@@ -0,0 +1,10 @@
+package com.journal.journalApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog {
+    public String fun(){
+        return "Dog is having fun!";
+    }
+}
--- src/main/java/com/journal/journalApp/MyClass.java
+++ src/main/java/com/journal/journalApp/MyClass.java
@@ -1,12 +1,17 @@
 package com.journal.journalApp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyClass {
-    @GetMapping("abc")
+
+    @Autowired
+    private Dog dog;
+
+    @GetMapping("/abc")
     public String sayHello(){
-         return "Hey! My name is Journal.";
+        return dog.fun();
     }
 }
